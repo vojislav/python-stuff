93d6e1acbcbf7bf0b4503c40cde91106771520e2
Vojzzo <voja.lazic122@gmail.com>
2017-04-14 21:28:00 +0200

    Edit

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,7 +1,7 @@
 import random
 
-secretNumber = random.randint(1, 5)
-print('Im thinking of a number between 1 and 5')
+secretNumber = random.randint(1, 20)
+print('Im thinking of a number between 1 and 20')
 
 for guessesTaken in range(1, 7):
     print('Make a guess')
