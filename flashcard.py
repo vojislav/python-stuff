5e4b1946aeb63e945ee2942df8c587cc52c59102
Voja Lazic <voja.lazic122@gmail.com>
2017-04-25 18:23:50 +0200

    Added flashcards script

---
+++ flashcard.py
@@ -0,0 +1,20 @@
+import random
+
+list = {}
+
+num = 1
+while True:
+	print ('Enter flashcard no.' + str(num))
+	ans = input()
+
+	if ans == 'exit':
+		quit()
+
+	if ans == 'help':
+		print('help')
+
+	if ans == 'show':
+		print('show')
+
+	if ans == 'return':
+		countinue.py
