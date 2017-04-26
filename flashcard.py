b3415078d10567653797d6a0e2b9585dea709fd2
Vojzzo <voja.lazic122@gmail.com>
2017-04-26 23:28:19 +0200

    Updated version of flashcards

--- flashcard.py
+++ flashcard.py
@@ -6,9 +6,35 @@ num = 0
 cor = 0
 wro = 0
 
+def adding():
+	global num
+	num = 0
+	for i in range(3):
+		print ('('+str(num)+') Enter question')
+		que = input()
+		question.append(que)
+		
+		print ('('+str(num)+') Enter answer')
+		ans = input()
+		answer.append(ans)
+		
+		print(question[num] + ' : ' + answer[num])
+		
+		num = num + 1
+		
+	print('----------Final list----------')
+	for i in range(num):
+		print(question[i] +' : '+ answer[i])
+		print('------------------------------')
+	questionTime()
+		
 def questionTime():
+	global cor
+	global wro
+	
 	cor = 0
 	wro = 0
+	print('----------Question time----------')
 	for i in range(num):
 		rand = random.randint(0, num-1)
 		print(question[rand])
@@ -29,24 +55,19 @@ def questionTime():
 		
 	else:
 		quit()
-	
-for i in range(3):
-	print ('('+str(num)+') Enter question')
-	que = input()
-	question.append(que)
-	
-	print ('('+str(num)+') Enter answer')
-	ans = input()
-	answer.append(ans)
-	
-	print(question[num] + ' : ' + answer[num])
-	num = num + 1
-	
-print('----------Final list----------')
-for i in range(num):
-	print(question[i] +' : '+ answer[i])
-	print('------------------------------')
+			
+def start():
+	num = 0
+	print('----------Welcome to Flashcard Maker----------')
+	print('Type:')
+	print('/start - to start making flashcards')
+	print('/quit - to quit')
+	start = input()
+	if start == '/start':
+		adding()
+	elif start == '/exit':
+		quit()
+	else:
+		start()
 
-print('----------Question time----------')
-	
-questionTime()
+start()
