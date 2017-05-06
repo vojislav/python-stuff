2483fe7b16ffe3cbdd473037e2f0c39741a0f296
Voja Lazic <voja.lazic122@gmail.com>
2017-05-06 14:06:56 +0200

    Added function to check letter

--- hangman.py
+++ hangman.py
@@ -1,14 +1,22 @@
 import random
 
-words = [#'plant', 'human', 'candy', 'dog', 'cat', 'string', 'death',
-    'coconut']
+words = ['plant', 'human', 'candy', 'dog', 'cat', 'string', 'death', 'coconut']
 num = 0
+alreadyIndexed = []
 
 print('Welcome to Hangman'.center(30, '-'))
 rand = random.randint(0, len(words) - 1)
 word = words[rand]
 hidWord = '-' * len(word)
 print(hidWord)
+
+def checkLetter():
+    global hidWord
+    letterIndex = word.index(letter)
+    hidWord = list(hidWord)
+    hidWord[letterIndex] = letter
+    hidWord = "".join(hidWord)
+    print(hidWord)
 
 for i in range(10):
     if '-' not in hidWord:
@@ -18,10 +26,8 @@ for i in range(10):
         print('Guess a letter or the whole word')
         letter = input()
         if str(letter) in word:
-            letterIndex = word.index(letter)
-            hidWord = list(hidWord)
-            hidWord[letterIndex] = letter
-            hidWord = "".join(hidWord)
-            print(hidWord)
+            numOfLetter = word.count(letter)
+            print(numOfLetter)
+            checkLetter()
         else:
             print('Wrong!')
