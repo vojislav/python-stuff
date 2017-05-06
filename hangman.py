ea5966a41487174499799d48728480ea74e77d69
Voja Lazic <voja.lazic122@gmail.com>
2017-05-06 19:56:07 +0200

    Added comments and updates

--- hangman.py
+++ hangman.py
@@ -1,44 +1,43 @@
-import random
-import re
+import random # for finding a random word
+import re # for finding all instances of a letter
 
 words = ['plant', 'human', 'candy', 'dog', 'cat', 'string', 'death', 'coconut',
-    'vacuum cleaner', 'ice cream']
+    'vacuum cleaner', 'ice cream'] # words to choose from
 
 print('Welcome to Hangman'.center(30, '-'))
-rand = random.randint(0, len(words) - 1)
-word = words[rand]
-if ' ' in word:
-    space = word.index(' ')
-    hidWord = list(word)
-    firstPart = hidWord[:space]
-    secondPart = hidWord[space:]
-    hidWord = '-' * len(firstPart) + ' ' + '-' * (len(secondPart) - 1)
+rand = random.randint(0, len(words) - 1) # random num for words list index
+word = words[rand] 
+if ' ' in word: # checking the word has a space (if it's a two-part word)
+    space = word.index(' ') # finding the index of the space in the word
+    firstPart = word[:space] # first half, from the beginning to the space
+    secondPart = word[space:] # second half, from the space to the end
+    hidWord = '-' * len(firstPart) + ' ' + '-' * (len(secondPart) - 1) # defining hidWord again as the firstPart in dashes and the secondPart in dashes
+                                                                       #(for some reason it kept adding another dash at the end of the secondPart, hence the - 1)
 else:
-    hidWord = '-' * len(word)
+    hidWord = '-' * len(word) # if not a two-part word
 print(hidWord)
 
-def checkLetter(letterIndex):
+def checkLetter(letterIndex): # defining func to check if letter is in word, with parameter letterIndex
     global hidWord
-    #letterIndex = word.index(letter)
     hidWord = list(hidWord)
     hidWord[letterIndex] = letter
     hidWord = "".join(hidWord)
 
-for i in range(10):
-    if '-' not in hidWord:
+for i in range(10): # loops 10 times
+    if '-' not in hidWord: # checking if done
         print('Congratz! You got it!')
         quit()
     else:
         print('Guess a letter or the whole word')
         letter = input()
-        charIndexes = ([m.start() for m in re.finditer(letter, word)])
-        if letter == word:
+        charIndexes = ([m.start() for m in re.finditer(letter, word)]) # I legit dunno how this works (looks for indexes of all instances of letter in word and places them in list charIndexes)
+        if letter == word: # checking if whole word guessed
              print('Congratz! You got it!')
              quit()
-        if str(letter) in word:
-            for i in charIndexes:
-                checkLetter(i)
+        elif letter in word: # if letter in word
+            for i in charIndexes: # loops for each i in charIndexes
+                checkLetter(i) # checkLetter func with parameter i (takes i, an index of a character, and uses it in place of letterIndex in function)
             print(hidWord)
         else:
-            print('Wrong!')
-print(word)
+            print('Wrong!') # if wrong
+print(word) # if not guessed within 10 turns, prints out word
