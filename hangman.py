f885f279b58a7ebcbeb232041d8d3e7973830827
Voja <voja.lazic122@gmail.com>
2017-05-08 16:57:50 +0200

    Added functions for everything

--- hangman.py
+++ hangman.py
@@ -5,40 +5,60 @@ words = ['plant', 'human', 'candy', 'dog', 'cat', 'string', 'death', 'coconut',
     'vacuum cleaner', 'ice cream'] # words to choose from
 
 print('Welcome to Hangman'.center(30, '-'))
-rand = random.randint(0, len(words) - 1) # random num for words list index
-word = words[rand] 
-if ' ' in word: # checking the word has a space (if it's a two-part word)
-    space = word.index(' ') # finding the index of the space in the word
-    firstPart = word[:space] # first half, from the beginning to the space
-    secondPart = word[space:] # second half, from the space to the end
-    hidWord = '-' * len(firstPart) + ' ' + '-' * (len(secondPart) - 1) # defining hidWord again as the firstPart in dashes and the secondPart in dashes
-                                                                       #(for some reason it kept adding another dash at the end of the secondPart, hence the - 1)
-else:
-    hidWord = '-' * len(word) # if not a two-part word
-print(hidWord)
 
+def start():
+    global hidWord
+    global word
+    rand = random.randint(0, len(words) - 1) # random num for words list index
+    word = words[rand] 
+    if ' ' in word: # checking the word has a space (if it's a two-part word)
+        space = word.index(' ') # finding the index of the space in the word
+        firstPart = word[:space] # first half, from the beginning to the space
+        secondPart = word[space:] # second half, from the space to the end
+        hidWord = '-' * len(firstPart) + ' ' + '-' * (len(secondPart) - 1) # defining hidWord again as the firstPart in dashes and the secondPart in dashes
+                                                                           #(for some reason it kept adding another dash at the end of the secondPart, hence the - 1)
+    else:
+        hidWord = '-' * len(word) # if not a two-part word
+    print(hidWord)
+
+def game():
+    global letter
+    
+    print('Guess a letter or the whole word')
+    letter = input()
+    charIndexes = ([m.start() for m in re.finditer(letter, word)]) # I legit dunno how this works 
+                                                                   #(looks for indexes of all instances of letter in word and places them in list charIndexes)
+    if letter == word: # checking if whole word guessed
+         print('Congratz! You got it!')
+         again()
+    elif letter in word: # if letter in word
+        for i in charIndexes: # loops for each i in charIndexes
+            checkLetter(i) # checkLetter func with parameter i (takes i, an index of a character, and uses it in place of letterIndex in function)
+        print(hidWord)
+    elif letter == ':':
+        print('Wrong!')
+    else:
+        print('Wrong!') # if wrong
+        
 def checkLetter(letterIndex): # defining func to check if letter is in word, with parameter letterIndex
     global hidWord
     hidWord = list(hidWord)
     hidWord[letterIndex] = letter
     hidWord = "".join(hidWord)
 
+def again():
+    print('Try again?(y/n)')
+    answer = input()
+    if answer == 'y':
+        start()
+    else:
+        quit()
+start()
 for i in range(10): # loops 10 times
     if '-' not in hidWord: # checking if done
         print('Congratz! You got it!')
-        quit()
+        again()
     else:
-        print('Guess a letter or the whole word')
-        letter = input()
-        charIndexes = ([m.start() for m in re.finditer(letter, word)]) # I legit dunno how this works 
-                                                                       #(looks for indexes of all instances of letter in word and places them in list charIndexes)
-        if letter == word: # checking if whole word guessed
-             print('Congratz! You got it!')
-             quit()
-        elif letter in word: # if letter in word
-            for i in charIndexes: # loops for each i in charIndexes
-                checkLetter(i) # checkLetter func with parameter i (takes i, an index of a character, and uses it in place of letterIndex in function)
-            print(hidWord)
-        else:
-            print('Wrong!') # if wrong
+        game()
 print(word) # if not guessed within 10 turns, prints out word
+again()
